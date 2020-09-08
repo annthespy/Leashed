53a81b6a4bc7f8e0edbc7d3f4174e2ee6a257cc3
Anna Makagonova <makagonova.ann@gmail.com>
2020-09-08 19:50:20 -0400

    set up server

---
+++ src/server/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const router = require("./router.js");
+
+const app = express();
+const PORT = 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static(__dirname + "/../client/dist"));
+
+app.use("/", router);
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
+});
