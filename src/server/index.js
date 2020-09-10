4eb3cc2534fe47672ff95c8869c6abb7f15f3475
Anna Makagonova <makagonova.ann@gmail.com>
2020-09-10 09:40:55 -0400

    fix the issue serving dist folder files

--- src/server/index.js
+++ src/server/index.js
@@ -1,16 +1,21 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const router = require("./router.js");
+const path = require("path");
+var morgan = require("morgan");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan("dev"));
 
-app.use(express.static(__dirname + "/../client/dist"));
+//app.use("/", express.static(path.join(__dirname + "../../dist")));
+//app.use("/", express.static("../../dist"));
+app.use(express.static("dist"));
 
-app.use("/", router);
+//app.use("/", router);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
