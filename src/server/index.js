c55cf51f5af66375dc637c870f940f166d17652a
Anna Makagonova <makagonova.ann@gmail.com>
2020-09-10 01:42:42 -0400

    change port and script

--- src/server/index.js
+++ src/server/index.js
@@ -3,7 +3,7 @@ const bodyParser = require("body-parser");
 const router = require("./router.js");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
